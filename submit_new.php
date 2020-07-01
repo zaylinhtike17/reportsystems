1152c5c74dd4c86eaa28ae4a0e68f169c6597c25
zaylinhtike17 <capital.zaylinhtike@gmail.com>
2020-07-01 09:58:45 +0630

    first commit

--- submit_new.php
+++ submit_new.php
@@ -13,6 +13,6 @@ $user_id=$_POST['id'];
 $pass=md5($_POST['password']);
 $query = "UPDATE  user_master set  password='$pass',updated_date=now() WHERE id='$user_id'";
 mysqli_query($conn, $query);
-header("location:login.php");
+header("location:success.php");
 
 ?>
